7f0b637a5ed075148ec23003041958df2c1baf4a
Nolan Calderon <nolan.calderon@ulatina.net>
2025-10-31 12:18:54 -0600

    feat(xml): mejora la extracción y procesamiento de información
    - Mejora la función de extracción y segmentación de tarifas de impuestos.
    - Optimiza la lógica de creación de tablas para visualizar la información extraída.
    - Limpia la estructura del HTML.

--- src/data_extraction_logic.js
+++ src/data_extraction_logic.js
@@ -1,16 +1,65 @@
-function getTableofContents(data) {
-  const { FacturaElectronica } = data || {};
-  
+function getTableofContents(DATA) {
+  const { FacturaElectronica } = DATA || {}
+  const proveedor = FacturaElectronica?.Emisor?.NombreComercial || 'Proveedor Desconocido'
   const listaServicios =  structuredClone(FacturaElectronica?.DetalleServicio?.LineaDetalle) || []
-  // Separamos los servicios por tarifa de impuesto
-  const listaImpuestos = listaServicios.reduce((acc, servicio) => {
-    const impuesto = servicio?.Impuesto?.Tarifa || []
-    if (!acc[impuesto]) acc[impuesto] = []
-    acc[impuesto].push(servicio)
+  
+  // Separamos los detalles por tarifa de impuesto
+  const listaImpuestos = listaServicios.reduce((acc, detalle) => {
+    // Linea de impuesto del detalle
+    const impuesto = detalle?.Impuesto?.Tarifa || {}
+
+    // Verificar si ya existe la tarifa de impuesto en el acumulador
+    if (!acc[impuesto]) acc[impuesto] = { servicios: [], totalTarifa: 0 }
+
+    // Sumar el monto total de la línea al total de tarifa del impuesto
+    const SumaTarifaTotal = parseFloat(acc[impuesto].totalTarifa) + (detalle?.MontoTotalLinea)
+    acc[impuesto].servicios.push(detalle)
+    acc[impuesto].totalTarifa = SumaTarifaTotal
+    
     return acc
   }, {})
-  return listaImpuestos;
-} 
+
+  // Generamos tabla de contenidos
+  const tablaContenidos = document.createElement('table')
+  tablaContenidos.classList.add('table-contents', 'table', 'table-bordered', 'mt-4')
+
+  // Encabezados de la tabla de contenidos
+  const tHeader = document.createElement('thead')
+  const headerRow = document.createElement('tr')
+
+  const th = document.createElement('th')
+  th.textContent = "NOMBRE PROVEEDOR"
+  headerRow.appendChild(th)
+ 
+  // Cuerpo de tabla de contenidos
+  const tbody = document.createElement('tbody')
+
+  // Mapeamos las filas y encabezados de la tabla de contenidos
+  const dataRow = document.createElement('tr')
+
+  // Nombre del proveedor
+  const tdProveedor = document.createElement('td')
+  tdProveedor.textContent = proveedor
+  dataRow.appendChild(tdProveedor)
+
+  Object.entries(listaImpuestos).forEach(([tarifa, info]) => {
+    // Encabezados
+    const th = document.createElement('th')
+    th.textContent = `Ventas al ${tarifa}%`
+    headerRow.appendChild(th)
+
+    // Datos
+    const tdDataTotals = document.createElement('td')
+    tdDataTotals.textContent = info.totalTarifa
+    dataRow.appendChild(tdDataTotals)
+    tbody.appendChild(dataRow)
+  })
+  tHeader.appendChild(headerRow)
+  tablaContenidos.appendChild(tHeader)
+  tablaContenidos.appendChild(tbody)
+
+  return tablaContenidos
+}
 
 
 export { getTableofContents }
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import { XMLParser } from "fast-xml-parser";
-import { getTableofContents } from "./data_extraction_logic.js";
+import { XMLParser } from "fast-xml-parser"
+import { getTableofContents } from "./data_extraction_logic.js"
 
-const parser = new XMLParser();
-const XMLFile = document.getElementsByClassName('xml_input_form')[0];
+const main_container = document.getElementsByClassName('main-container')[0]
+
+const parser = new XMLParser()
+const XMLFile = document.getElementsByClassName('xml_input_form')[0]
 
 XMLFile.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+  const file = event.target.files[0]
+  const reader = new FileReader()
   reader.onload = function (e) {
-    const content = e.target.result;
-    const result = parser.parse(content);
-    console.log('Parsed XML object:', result);
-    const toc = getTableofContents(result);
-    console.log('Table of Contents:', toc);
+    const content = e.target.result
+    const result = parser.parse(content)
+    const toc = getTableofContents(result)
+    main_container.appendChild(toc)
   }
-  reader.readAsText(file);
-});
+  reader.readAsText(file)
+})
 
