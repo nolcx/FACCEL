9bfc8fb194ff482062f5f3da16da2b1d0e82951a
Nolan Calderon <nolan.calderon@ulatina.net>
2025-12-14 13:08:48 -0600

    feat: Mejora la lógica de manejo de fechas, agregando verificación de igualdad y ajuste en el formato de salida
    - Se ajusta la fecha de retorno con formato "YYYY/MM/DD HH:mm:ss"; para corregir el bug de día restado en SheetJS.
    - Se mejora logica de validación de fechas iguales y entre fechas; para evitar tomar en cuenta la hora y zona horaria de la fecha.

--- src/Utils/DateOperations.js
+++ src/Utils/DateOperations.js
@@ -1,14 +1,27 @@
 import dayjs from 'dayjs'
+import isBetween from 'dayjs/plugin/isBetween'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 dayjs.extend(customParseFormat)
+dayjs.extend(isBetween)
 
+// Función para parsear una fecha a formato 'YYYY/MM/DD HH:mm:ss'
 function parseDate (date) {
-  return dayjs(date).format('YYYY/MM/DD')
+  return dayjs(date).format('YYYY/MM/DD HH:mm:ss')
 }
 
+// Verificar si una fecha es la misma que otra fecha (sin considerar la hora)
+function isSameDate (date1, date2) {
+  return dayjs(date1).startOf('day')
+    .isSame(dayjs(date2).startOf('day'))
+}
+
+// Función para verificar si una fecha está entre dos fechas dadas (sin considerar la hora)
 function isBetweenDates (date, startDate, endDate) {
-  const d = dayjs(date)
-  return d.isAfter(dayjs(startDate)) && d.isBefore(dayjs(endDate))
+  const d = dayjs(date).startOf('day')
+  const start = dayjs(startDate).startOf('day')
+  const end = dayjs(endDate).endOf('day')
+
+  return d.isBetween(start, end, null, '[]')
 }
 
-export { parseDate, isBetweenDates }
+export { parseDate, isSameDate, isBetweenDates }
