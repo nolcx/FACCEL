f0823be0a66943d7632270e36f2058182e16d1a3
Nolan Calderon <nolan.calderon@ulatina.net>
2025-12-14 15:58:11 -0600

    feat: Agregar notificaciones de Toastify para mensajes de advertencia, éxito e información
    - Se agrega libreria Toastify-JS al proyecto.
    - Se crean funciones para mostrar Toastifys con mensajes personalizados.

---
+++ src/Components/Toastifys.js
@@ -0,0 +1,49 @@
+import Toastify from 'toastify-js'
+import 'toastify-js/src/toastify.css'
+
+function WarningToasty (detalle) {
+  Toastify({
+    text: `⚠️ ${detalle} `,
+    duration: 4000,
+    close: true,
+    gravity: 'top',
+    position: 'right',
+    style: {
+      color: '#333333ff',
+      background: '#e9e3afff',
+      borderRadius: '8px'
+    }
+  }).showToast()
+}
+
+function SuccessToasty (detalle) {
+  Toastify({
+    text: `✅ ${detalle} `,
+    duration: 4000,
+    close: true,
+    gravity: 'top',
+    position: 'right',
+    style: {
+      color: '#333333ff',
+      background: '#d1e7dd',
+      borderRadius: '8px'
+    }
+  }).showToast()
+}
+
+function InfoToasty (detalle) {
+  Toastify({
+    text: `ℹ️ ${detalle} `,
+    duration: 4000,
+    close: true,
+    gravity: 'top',
+    position: 'right',
+    style: {
+      color: '#333333ff',
+      background: '#cfe2ff',
+      borderRadius: '8px'
+    }
+  }).showToast()
+}
+
+export { WarningToasty, SuccessToasty, InfoToasty }
